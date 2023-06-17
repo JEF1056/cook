38774369645bce421b464fe24563261609435a16
Jess <jess.fan@potatolondon.com>
2023-06-17 10:37:15 -0700

    initial setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}", 'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js']],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
---
+++ node_modules/react-daisyui/dist/Button/index.d.ts
@@ -0,0 +1,3 @@
+import Button, { ButtonProps as TButtonProps } from './Button';
+export type ButtonProps = TButtonProps;
+export default Button;
