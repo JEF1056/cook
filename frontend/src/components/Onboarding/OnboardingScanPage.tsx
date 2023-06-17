1da8291b1eaf83a1e510b586b23191eee87eb34b
Jess <jess.fan@potatolondon.com>
2023-06-17 13:56:55 -0700

    Scan page :thumbsup:

--- frontend/src/components/Onboarding/OnboardingScanPage.tsx
+++ frontend/src/components/Onboarding/OnboardingScanPage.tsx
@@ -1,14 +1,16 @@
 import { StepsComponent } from "./StepsComponent";
 import { useRef, useState, useCallback } from "react";
+import { useWindowSize } from "../../services/hooks";
 import Webcam from "react-webcam";
 
 function OnboardingScanPage() {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
+  const size = useWindowSize();
 
-  const videoConstraints = {
-    width: window.innerWidth,
-    height: window.innerHeight,
+  const videoConstraints: MediaTrackConstraints = {
+    aspectRatio: size.ratio,
+    facingMode: "environment",
   };
 
   //   const capture = useCallback(() => {
@@ -18,18 +20,26 @@ function OnboardingScanPage() {
   //     }
   //   }, [webcamRef, setImgSrc]);
 
+  console.log(size);
+
   return (
-    <div>
-      <div className="flex flex-row absolute top-10 justify-center w-full px-2">
+    <div className="flex items-center justify-center">
+      <div className="absolute top-5 bg-neutral bg-opacity-80 rounded-lg w-96 lg:w-128 p-2">
         <StepsComponent currentStep={0} />
       </div>
 
       <Webcam
         audio={false}
         ref={webcamRef}
+        width={size.width}
+        height={size.height}
         screenshotFormat="image/jpeg"
         videoConstraints={videoConstraints}
       />
+
+      <div className="flex flex-row absolute bottom-10 justify-center w-full">
+        <button className="btn btn-primary">Scan</button>
+      </div>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}", 'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
+  content: [
+    "./src/**/*.{html,js,ts,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+    },
   },
   plugins: [require("daisyui")],
-}
-
+};
