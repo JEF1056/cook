4f46a6fb4dfa1362411adb658a5b851531f68ca2
Zackary Oon <zackaryoon134@berkeley.edu>
2023-06-17 11:12:22 -0700

    Create components folder, services folder, install json-server for testing

---
+++ frontend/src/components/Welcome.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const inputFood = () => {
+}
+
+const Welcome = () => {
+
+}
+
+export default Welcome
---
+++ frontend/src/services/food.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/ingredients'
+
+const getAllIngredients = () => {
+    //const request = axios.get()
+}
+
+// export default { getAllIngredients }
