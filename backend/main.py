50c191c2777df0e5d8ff6eb901e1903fcf29bf0b
Jess <jess.fan@potatolondon.com>
2023-06-18 00:23:14 -0700

    cors fixes

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import openai
 import dataset
-from flask import request
+from flask_cors import CORS
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
+CORS(app)
 
-db = dataset.connect('sqlite:///cook.db')
+db = dataset.connect("sqlite:///cook.db")
 
 
 @app.route("/")
@@ -51,7 +56,7 @@ def insert_recipe(table):
 
 @app.route("/ingredients")
 def ingredients():
-    return list(db.get_table('ingredients'))
+    return list(db.get_table("ingredients"))
 
 
 @app.route("/ingredients/<ingredient>")
@@ -63,4 +68,30 @@ def ingredient(ingredient):
         return "error"
 
 
-app.run(port=5000, debug=True)
+@app.route("/get_image")
+def get_image():
+    prompt = request.args.get("prompt")
+    if prompt is None:
+        return "error: no prompt", 400
+    cached_image = db["images"].find_one(prompt=request.args.get("prompt"))
+    print(cached_image)
+    if cached_image is not None:
+        b64 = cached_image["b64_json"]
+        # db["images"].delete_one(prompt=request.args.get("prompt"))
+        return b64
+    else:
+        response = openai.Image.create(
+            prompt=prompt, n=1, size="512x512", response_format="b64_json"
+        )
+
+        # db["images"].insert_many(
+        #     [
+        #         {"prompt": prompt, "b64_json": resp["b64_json"]}
+        #         for resp in response["data"][1:]
+        #     ]
+        # )
+
+        return response["data"][0]["b64_json"]
+
+
+app.run(port=5000, debug=False, threaded=False)
